d7ab8272165bdabeff249afc3bfd26a4fbd73a9f
topisteronyango <t.onyango@alustudent.com>
2022-04-06 15:38:31 -0700

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleSignUp() {
-  Promise.all([uploadPhoto(), createUser()]).then(
-    (results) => {
-      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-    }, () => {
-      console.log('Signup system offline');
-    },
-  );
+export default function handleProfileSignup() {
+  return Promise.all([
+    uploadPhoto(),
+    createUser(),
+  ]).then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  }).catch(() => console.log('Signup system offline'));
 }
