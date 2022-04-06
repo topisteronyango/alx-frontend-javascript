1f142f6530b8f3539ca860be19234665a31e25d9
topisteronyango <t.onyango@alustudent.com>
2022-04-06 15:36:28 -0700

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,8 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
     body: 'success',
-  }))
-  .catch(() => Error())
-  .finally(() => console.log('Got a response from the API'));
+  })).catch(() => Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
