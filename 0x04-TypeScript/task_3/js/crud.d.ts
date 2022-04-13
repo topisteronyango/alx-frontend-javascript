62cec9f472191d098b5213e0d878aa265d435d56
topisteronyango <t.onyango@alustudent.com>
2022-04-13 13:14:12 +0200

    delete

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++
@@ -1,7 +0,0 @@
-import { RowID, RowElement } from './interface.ts';
-
-export type insertRow = (row: RowElement) => number;
-
-export type deleteRow = (rowId: RowID) => void;
-
-export type updateRow = (rowId: RowID, row: RowElement) => RowID;
